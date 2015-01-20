aba10d32a9c0850f0728c27f9e6a1b739c7015b8
vanessadean <vanessa.dean@gmail.com>
2015-01-20 13:56:32 -0500

    add search by tag

--- foursquare.rb
+++ foursquare.rb
@@ -2,58 +2,73 @@ require 'httparty'
 
 class Neighborhood
   
-  attr_accessor :location, :venues, :correct_venues, :group
+  attr_accessor :location, :recommended_venues, :venue_ids, :venues_to_search, :venues_by_group, :venues_by_tag
   
   ##!!!NEEDS TO BE FILLED OUT WITH CLIENT ID AND SECRET PROVIDED BY FOURSQUARE
-  # CLIENT_SECRET = 
-  # CLIENT_ID = 
-  def initialize(location, group)
+  CLIENT_ID = "JUUFHYCI1ZWKTMHF5GEG1ZODCTREEO0TJRCC02UPOYCYJIGB"
+  CLIENT_SECRET = "GYUAW432FZ1UJ4PX4TM3IGSYNO2QAHMNAV4OR2DUKCHMJULJ"
+
+  def initialize(location)
     @location = location
-    @correct_venues = []
-    @venues = []
-    @group = group
+    @recommended_venues = get_recommended_venues
+    @venue_ids = []
+    @venues_to_search = []
+    @venues_by_group = []
+    @venues_by_tag = []
   end
 
-  def search
+  # This user the Foursquare explore endpoint to pull recommended food venues for a location
+  def get_recommended_venues
     uri = "https://api.foursquare.com/v2/venues/explore?near=#{@location}&client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{Time.now.strftime("%Y%m%d")}&categoryId=4d4b7105d754a06374d81259"
-    encoded = URI.parse(URI.encode(uri)) #to handle spaces in the location
-    @venues = HTTParty.get(encoded)['response']['groups'][0]["items"]
+    encoded = URI.parse(URI.encode(uri)) # to handle spaces in the location
+    api_response = HTTParty.get(encoded)
+    @recommended_venues = api_response['response']['groups'][0]["items"]
   end
+  # Example uri https://api.foursquare.com/v2/venues/explore?near=East%20Village,%20New%20York,%20NY&client_id=JUUFHYCI1ZWKTMHF5GEG1ZODCTREEO0TJRCC02UPOYCYJIGB&client_secret=GYUAW432FZ1UJ4PX4TM3IGSYNO2QAHMNAV4OR2DUKCHMJULJ&v=20150120&categoryId=4d4b7105d754a06374d81259
 
+  # Our recommended venue list doesn't have all the info we need to search by group or tag so we need to make an API call for more info on each venue. We need the venue ids to do that.
   def get_venue_ids
-    ids = []
-    @venues.each do |venue|
-      ids << venue["venue"]["id"]
+    @recommended_venues.each do |venue| 
+      @venue_ids << venue["venue"]["id"] 
     end
-    @venues = []
-    ids.each do |i|
-      @venues << HTTParty.get("https://api.foursquare.com/v2/venues/#{i}?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{Time.now.strftime("%Y%m%d")}&m=foursquare")
-    end
+    @venue_ids
   end
 
-  def check_group
-    @correct_venues = []
-    @venues.each do |venue|
-      venue['response']['venue']['attributes']['groups'].each do |v|
-        v.each do |info, value|
-          if info == "items" && value.first["displayName"] == @group
-            if info == "items" && value.first["displayValue"].split(" ").first == "Yes" 
-              @correct_venues << venue['response']['venue']['name']
-            elsif value.first["displayName"] == value.first["displayValue"]
-              @correct_venues << venue['response']['venue']['name']
+  def get_venues_to_search
+    @venue_ids.each do |id|
+      api_response = HTTParty.get("https://api.foursquare.com/v2/venues/#{id}?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{Time.now.strftime("%Y%m%d")}&m=foursquare")
+      # puts api_response['response']['venue']
+      @venues_to_search << api_response['response']['venue']
+    end
+    @venues_to_search
+  end
+  # Example uri: https://api.foursquare.com/v2/venues/4acca438f964a5201dc920e3?client_id=JUUFHYCI1ZWKTMHF5GEG1ZODCTREEO0TJRCC02UPOYCYJIGB&client_secret=GYUAW432FZ1UJ4PX4TM3IGSYNO2QAHMNAV4OR2DUKCHMJULJ&v=20150120&m=foursquare
+
+  # Groups to search by include ["outdoor seating","payments","price","reservations","music","drinks","dining options","parking","wheelchair accessible" ]
+  def search_by_group(group="Outdoor Seating")
+    @venues_to_search.each do |venue|
+      venue['attributes']['groups'].each do |groups|
+        if groups["name"].downcase == group.downcase
+          groups["items"].each do |item|
+            if item["displayValue"].split(" ").first != "No"
+              @venues_by_group << venue["name"]
             end
-           elsif info == "items" && value.length > 1
-              value.each do |v|
-                if v["displayName"] == v["displayValue"] && v["displayValue"] == @group
-                  @correct_venues << venue['response']['venue']['name']
-                end
-              end
           end
         end
-      end
+      end  
     end
-    @correct_venues.uniq!
-    @correct_venues
+    @venues_by_group
+  end
+
+  # Here is an example list of tags from Momofuku Ssam Bar ["david chang","pork","pork buns","spicy rice cakes","steamed buns","trendy","zagat-rated"]
+  def search_by_tag(tag="steamed buns")
+    @venues_to_search.each do |venue|
+      venue["tags"].each do |venue_tag|
+        if venue_tag.downcase == tag.downcase
+          @venues_by_tag << venue["name"]
+        end
+      end  
+    end
   end
 
 end
