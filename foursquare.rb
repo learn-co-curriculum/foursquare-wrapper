97172a9469641d52f6f4ba52321e560587fc5af5
vanessadean <vanessa.dean@gmail.com>
2015-01-26 10:47:35 -0500

    Edits

--- foursquare.rb
+++ foursquare.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 class Neighborhood
   
-  attr_accessor :location, :recommended_venues, :venue_ids, :venues_to_search, :venues_by_group, :venues_by_tag, :api_response
+  attr_accessor :location, :recommended_venues, :venue_ids, :venues_for_search, :venues_by_group, :api_response
   
   ##!!!NEEDS TO BE FILLED OUT WITH CLIENT ID AND SECRET PROVIDED BY FOURSQUARE
   # CLIENT_ID = ""
@@ -12,9 +12,8 @@ class Neighborhood
     @location = location
     @recommended_venues = []
     @venue_ids = []
-    @venues_to_search = []
+    @venues_for_search = []
     @venues_by_group = []
-    @venues_by_tag = []
   end
 
   # This user the Foursquare explore endpoint to pull recommended food venues for a location
@@ -36,19 +35,19 @@ class Neighborhood
     @venue_ids
   end
 
-  def get_venues_to_search
+  def get_venues_for_search
     @venue_ids.each do |id|
       uri = "https://api.foursquare.com/v2/venues/#{id}?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{Time.now.strftime("%Y%m%d")}&m=foursquare"
       api_response = HTTParty.get(uri)
-      # puts uri
-      @venues_to_search << api_response['response']['venue']
+      puts uri
+      @venues_for_search << api_response['response']['venue']
     end
-    @venues_to_search
+    @venues_for_search
   end
 
   # Example groups to search by include ["outdoor seating","payments","price","reservations","music","drinks","dining options","parking","wheelchair accessible" ]
-  def search_by_group(group="Outdoor Seating")
-    @venues_to_search.each do |venue|
+  def filter_by_group(group="Outdoor Seating")
+    @venues_for_search.each do |venue|
       venue['attributes']['groups'].each do |groups|
         if groups["name"].downcase == group.downcase
           groups["items"].each do |item|
